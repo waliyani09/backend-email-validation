c596c4853c05fc287205fd188c56c447bd1a0d46
Azim Waliyani <waliyani09@gmail.com>
2023-02-12 21:56:39 -0500

    Delete index.js

--- db/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://plasmaofspeed:vpFMAQOj9XUK48QO@cluster0.rksojkw.mongodb.net/test').then(()=>{
-    console.log('db is connected');
-}).catch((ex)=>{
-    console.log('Database connection  failed: ',ex);
-})
